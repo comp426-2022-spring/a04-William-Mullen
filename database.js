a67bd3f63e9d47a0220b2eaf3bef5becd3ed883c
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-04-24 13:29:37 -0400

    table names and checking create?

--- database.js
+++ database.js
@@ -11,11 +11,13 @@ if (row == undefined) {
     console.log('Log database appears to be empty. Creating log database...')
 
     const sqlInit = `
-    CREATE TABLE accesslog ( id INTERGER PRIMARY KEY, remote-addr VARCHAR, remote-user VARCHAR, datetime VARCHAR, method VARCHAR 
-        url VARCHAR, protocol VARCHAR, http-version NUMERIC, status INTEGER, referer VARCHAR, user-agent VARCHAR)
-    `
+    CREATE TABLE accesslog ( id INTERGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR 
+        url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR);
+    `;
 
     logdp.exec(sqlInit)
+
+    console.log ('created database?')
 }
 else {
     console.log('Log database exists.')
